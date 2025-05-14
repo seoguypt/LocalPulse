cd1e8f8987d4140d2cb8cf1eabf7f2cbb6598497
Andre van Tonder <andre@drevan.me>
2025-05-14 21:04:48 +1000

    Remove unused NitroApp type declaration from nitro.d.ts, streamlining the codebase.

--- types/nitro.d.ts
+++
@@ -1,8 +0,0 @@
-import type { NitroApp } from 'nitropack';
-import { Camoufox } from 'camoufox-js';
-
-declare module 'nitropack' {
-  interface NitroApp {
-    browser: Awaited<ReturnType<typeof Camoufox>>;
-  }
-}
