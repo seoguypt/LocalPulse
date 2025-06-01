c9c8d46226a2f036b06cfe1ed9224e7539898818
Andre van Tonder <andre@drevan.me>
2025-06-01 23:52:35 +1000

    fix: Improve performance of logo

--- app/components/TheLogo.vue
+++ app/components/TheLogo.vue
@@ -1,6 +1,6 @@
 <template>
   <NuxtLink to="/" class="text-lg font-bold font-display tracking-tight flex items-center gap-2">
-    <NuxtImg src="/logo.png" alt="VisiMate" class="w-8 h-8 -mt-1" />
+    <NuxtImg format="webp" src="/logo.png" alt="An icon of a magical eye in V" class="w-8 h-8 -mt-1" width="32" height="32" />
     <span>VisiMate<span class="text-sm">.au</span></span>
   </NuxtLink>
 </template>
