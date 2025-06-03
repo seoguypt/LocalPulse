3a17d4d8ebe347b3d2e56b9b5e84cb644e55c5fb
Andre van Tonder <andre@drevan.me>
2025-06-03 18:17:32 +1000

    fix: Badge in logo should use `font-sans`

--- app/components/TheLogo.vue
+++ app/components/TheLogo.vue
@@ -2,6 +2,6 @@
   <NuxtLink to="/" class="text-lg font-bold font-display tracking-tight flex items-center gap-2">
     <NuxtImg format="webp" src="/logo.png" alt="An icon of a magical eye in V" class="w-8 h-8 -mt-1" width="32" height="32" />
     <span>VisiMate<span class="text-sm">.au</span></span>
-    <UBadge color="primary" variant="subtle">Alpha</UBadge>
+    <UBadge color="primary" variant="subtle" class="font-sans">Alpha</UBadge>
   </NuxtLink>
 </template>
